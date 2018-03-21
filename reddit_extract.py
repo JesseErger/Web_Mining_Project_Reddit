b3b6e1147b65ea7e695be655bea2d92c2e5ef89b
JesseErger <jessey445@hotmail.com>
2018-03-20 23:31:11 -0500

    Comments in the wrong spot for pprint on the submission object

--- reddit_extract.py
+++ reddit_extract.py
@@ -10,25 +10,23 @@ reddit = praw.Reddit(client_id='0RdYJQZXJWJd6Q',
 
 
 #An example of one submission's comments. We can extract all the post ids from the log files but it seems like A LOT of them are deleted. I'm not sure if my credentials will work for you above where I set up the reddit authorization, and I'm not sure what their limits are on extracting this data. I'm sure there is a lot more we can do with the API but figured I would just get something started.                     
-                     
-
-                     
+                                        
 #Can get all the titles of a sub-reddit just up the limit
 for submission in reddit.subreddit('depression').hot(limit=10):
    print("Author ->" + str(submission.author))
    print(submission.title)
 
+   
 #get a single submission    
 print("_____________________________________SUBMISSION PARAMETERS_____________________________________")
 submission = reddit.submission(id='5lpt8d') 
-print("_________________________________END SUBMISSION PARAMETERS_____________________________________")
-
 #You can use this to figure out all of the available fields from a parameter
 pprint.pprint(vars(submission))
+print("_________________________________END SUBMISSION PARAMETERS_____________________________________")
+
 
 submission_author = str(submission.author)
 print("Author of 5lpt8d -> " +  str(submission.author))
-
 
 #Get top level comments in a thread
 print("Top level comments from 5lpt8d , author ")
